ab4c96f15be0ed610ebd10408d9a6a227c1109fb
itsnadh <itsnadzakia@gmail.com>
2022-03-31 22:07:03 +0700

    upload file

---
+++ script.js
@@ -0,0 +1,62 @@
+// const calculatorscreen = document.querySelector('.calculator-screen')
+
+// const updateScreen = (number) => {
+//     calculatorscreen.value = number
+// }
+
+const numbers = document.querySelectorAll(".number")
+
+console.log(numbers)
+
+// numbers.forEach((number)=> {
+//     number.addEventListener("click", (event) =>{
+//         updateScreen(event.target.value)
+//     })
+// })
+
+// let prevNumber = ''
+// let calculationOperation = ''
+// let currentNumber ='0'
+
+// numbers.forEach((number)=> {
+//     number.addEventListener("click", (event) =>{
+//         inputNumber(event.target.value)
+//         updateScreen(currentNumber)
+//     })
+// })
+
+// const inputNumber = (number) =>{
+//     if (currentNumber === '0') {
+//         currentNumber = number
+//     } else {
+//         currentNumber += number
+//     }
+// }
+
+// const operators = document.querySelectorAll(".operator")
+
+// operators.forEach((operator)=>{
+//     operator.addEventListener("click", (svent) => {
+//         inputOperator(svent.target.value)
+//     })
+// })
+
+// const inputOperator = (operator) => {
+//     prevNumber = currentNumber
+//     calculationOperation = operator
+//     currentNumber = ''
+// }
+
+// const equalSign = document.querySelector('.equal-sign')
+
+// equalSign.addEventListener("click", () =>{
+//     console.log('equal button')
+// })
+
+
+
+
+
+
+
+
